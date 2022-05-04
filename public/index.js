8ebc70aa42db8085069f413d3fb969c43f560975
Chris L <chris.ladden@gmail.com>
2022-05-03 21:12:09 -0500

    saved current increment or decrement after refresh

--- public/index.js
+++ public/index.js
@@ -3,7 +3,12 @@ async function main(){
     const incrementButton = document.querySelector('#increment-button');
     const decrementButton = document.querySelector('#decrement-button');
 
-    const response = await fetch('http://localhost:9001/counter');
+    const response = await fetch('http://localhost:9001/counter', {
+        method: 'PATCH',
+        body: JSON.stringify({
+            "value": 'countValue'
+        })
+    })
 
     const result = await response.json();
     
@@ -12,11 +17,31 @@ async function main(){
     function increment(){
         countValue++;
         countContainer.textContent = countValue;
+
+        fetch('http://localhost:9001/counter', {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                value: countValue
+            })
+        })
     }
 
     function decrement(){
         countValue--;
         countContainer.textContent = countValue;
+
+        fetch('http://localhost:9001/counter', {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                value: countValue
+            })
+        })
     }
 
     incrementButton.addEventListener('click', increment);
